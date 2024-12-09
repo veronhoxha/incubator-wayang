e346ad47491e7532663a06f3b7e5f252aac91bf0
Veron <veronbhoxha@gmail.com>
2024-12-09 14:47:28 +0100

    job updated

--- wayang-benchmark/src/main/java/org/apache/wayang/apps/wordcount/ParquetWordCountJob.java
+++ wayang-benchmark/src/main/java/org/apache/wayang/apps/wordcount/ParquetWordCountJob.java
@@ -65,17 +65,20 @@ public class ParquetWordCountJob {
             "}"
         );
 
+        // tried to implement the pushdown projection but it didn't work as expected so just leaving it here as a comment
+
+
         // yelp review record schema with label as optional
-        Schema partialSchema = new Schema.Parser().parse(
-            "{\n" +
-            "  \"type\":\"record\",\n" +
-            "  \"name\":\"ReviewRecord\",\n" +
-            "  \"fields\":[\n" +
-            "    {\"name\":\"label\", \"type\":[\"null\",\"long\"], \"default\":null},\n" +
-            "    {\"name\":\"text\", \"type\":\"string\"}\n" +
-            "  ]\n" +
-            "}"
-        );
+        // Schema partialSchema = new Schema.Parser().parse(
+        //     "{\n" +
+        //     "  \"type\":\"record\",\n" +
+        //     "  \"name\":\"ReviewRecord\",\n" +
+        //     "  \"fields\":[\n" +
+        //     // "    {\"name\":\"label\", \"type\":[\"null\",\"long\"], \"default\":null},\n" +
+        //     "    {\"name\":\"text\", \"type\":\"string\"}\n" +
+        //     "  ]\n" +
+        //     "}"
+        // );
 
         // 6 total runs: 1 warm-up run (not recorded) + 5 measured runs
         int totalRuns = 6;
@@ -185,57 +188,57 @@ public class ParquetWordCountJob {
         double avgParquetFullTime = Arrays.stream(parquetFullTimes).average().orElse(Double.NaN);
 
 
-        // parquet partial schema - projection push down
-        long[] parquetPartialTimes = new long[measuredRuns];
+        // // parquet partial schema - projection push down
+        // long[] parquetPartialTimes = new long[measuredRuns];
 
-        // warm-up run parquet partial - projection push down
-        new JavaPlanBuilder(wayangContext)
-                .withJobName("WordCount-ParquetPartial-Warmup")
-                .withUdfJarOf(ParquetWordCountJob.class)
-                .readParquetFile(parquetInput, partialSchema)
-                .flatMap(record -> Arrays.asList(record.get("text").toString().split("\\W+")))
-                .filter(token -> !token.isEmpty())
-                .map(word -> new Tuple2<>(word.toLowerCase(), 1))
-                .reduceByKey(
-                        Tuple2::getField0,
-                        (t1, t2) -> new Tuple2<>(t1.getField0(), t1.getField1() + t2.getField1())
-                )
-                .collect();
+        // // warm-up run parquet partial - projection push down
+        // new JavaPlanBuilder(wayangContext)
+        //         .withJobName("WordCount-ParquetPartial-Warmup")
+        //         .withUdfJarOf(ParquetWordCountJob.class)
+        //         .readParquetFile(parquetInput, partialSchema)
+        //         .flatMap(record -> Arrays.asList(record.get("text").toString().split("\\W+")))
+        //         .filter(token -> !token.isEmpty())
+        //         .map(word -> new Tuple2<>(word.toLowerCase(), 1))
+        //         .reduceByKey(
+        //                 Tuple2::getField0,
+        //                 (t1, t2) -> new Tuple2<>(t1.getField0(), t1.getField1() + t2.getField1())
+        //         )
+        //         .collect();
 
-        // measured runs parquet partial - projection push down
-        for (int i = 0; i < measuredRuns; i++) {
-            JavaPlanBuilder planBuilder = new JavaPlanBuilder(wayangContext)
-                    .withJobName("WordCount-ParquetPartial-Run" + (i + 1))
-                    .withUdfJarOf(ParquetWordCountJob.class);
+        // // measured runs parquet partial - projection push down
+        // for (int i = 0; i < measuredRuns; i++) {
+        //     JavaPlanBuilder planBuilder = new JavaPlanBuilder(wayangContext)
+        //             .withJobName("WordCount-ParquetPartial-Run" + (i + 1))
+        //             .withUdfJarOf(ParquetWordCountJob.class);
 
-            long start = System.currentTimeMillis();
-            Collection<Tuple2<String, Integer>> result = planBuilder
-                    .readParquetFile(parquetInput, partialSchema)
-                    .flatMap(record -> Arrays.asList(record.get("text").toString().split("\\W+")))
-                    .filter(token -> !token.isEmpty())
-                    .map(word -> new Tuple2<>(word.toLowerCase(), 1))
-                    .reduceByKey(
-                            Tuple2::getField0,
-                            (t1, t2) -> new Tuple2<>(t1.getField0(), t1.getField1() + t2.getField1())
-                    )
-                    .collect();
-            long end = System.currentTimeMillis();
-            parquetPartialTimes[i] = (end - start);
+        //     long start = System.currentTimeMillis();
+        //     Collection<Tuple2<String, Integer>> result = planBuilder
+        //             .readParquetFile(parquetInput, partialSchema)
+        //             .flatMap(record -> Arrays.asList(record.get("text").toString().split("\\W+")))
+        //             .filter(token -> !token.isEmpty())
+        //             .map(word -> new Tuple2<>(word.toLowerCase(), 1))
+        //             .reduceByKey(
+        //                     Tuple2::getField0,
+        //                     (t1, t2) -> new Tuple2<>(t1.getField0(), t1.getField1() + t2.getField1())
+        //             )
+        //             .collect();
+        //     long end = System.currentTimeMillis();
+        //     parquetPartialTimes[i] = (end - start);
 
-            System.out.printf("Found %d words from Parquet Partial run %d\n", result.size(), i+1);
-            System.out.println("Parquet Partial Schema Run " + (i + 1) + ": " + parquetPartialTimes[i] + " ms");
+        //     System.out.printf("Found %d words from Parquet Partial run %d\n", result.size(), i+1);
+        //     System.out.println("Parquet Partial Schema Run " + (i + 1) + ": " + parquetPartialTimes[i] + " ms");
             
-            // uncomment the following line to print out the words
-            // result.forEach(wc -> System.out.printf("%dx %s\n", wc.field1, wc.field0));
-            // System.out.println();
-        }
-        double avgParquetPartialTime = Arrays.stream(parquetPartialTimes).average().orElse(Double.NaN);
+        //     // uncomment the following line to print out the words
+        //     // result.forEach(wc -> System.out.printf("%dx %s\n", wc.field1, wc.field0));
+        //     // System.out.println();
+        // }
+        // double avgParquetPartialTime = Arrays.stream(parquetPartialTimes).average().orElse(Double.NaN);
 
         // average times over the measured runs
         System.out.println("\n=== Averages (based on 5 measured runs) ===");
-        System.out.println("Average CSV Processing Time: " + avgCsvTime + " ms");
-        System.out.println("Average Parquet (full schema) Processing Time: " + avgParquetFullTime + " ms");
-        System.out.println("Average Parquet (partial schema) Processing Time: " + avgParquetPartialTime + " ms");
+        System.out.println("Average CSV Runtime Time: " + avgCsvTime + " ms");
+        System.out.println("Average Parquet (full schema) Runtime Time: " + avgParquetFullTime + " ms");
+        // System.out.println("Average Parquet (partial schema) Processing Time: " + avgParquetPartialTime + " ms");
         System.out.println();
         // System.out.println("New experiment starting...");
         System.out.println();
